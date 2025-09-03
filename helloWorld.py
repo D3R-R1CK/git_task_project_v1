1197570628181d8309fab9d4bd4837cc4b1c68ba
Cian Carr <cian.carr2@gmail.com>
2025-09-03 13:47:39 +0200

    Add explanatory comments to helloWorld.py for clarity (Fixes #issue1)

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,3 @@
+#Ask user for input(Their name) which will be printed after input is given
 user_input = input("Please enter your name :")
-print(user_input)
+print(user_input)  # Print the input that was given by the user
