61acc64f63a9ce872f58a6c6a0d5058d100166f9
Cian Carr <cian.carr2@gmail.com>
2025-09-03 14:15:57 +0200

    Add personalised greeting using user input in helloWorld.py (Fixes #issue2)

--- helloWorld.py
+++ helloWorld.py
@@ -1,2 +1,3 @@
+# Ask for user input that will be used for greeting
 user_input = input("Please enter your name :")
-print(user_input)
+print(f"Hello, {user_input}! Git is Awesome !")  # Use user input to print out greeting
